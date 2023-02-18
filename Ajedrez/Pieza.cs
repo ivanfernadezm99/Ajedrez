bc43a0988c13f98b370ef2d253dce3decc7b490f
Ivan <ivanfernandezm99@gmail.com>
2023-02-18 01:11:32 -0300

    Agregar archivos de proyecto.

---
+++ Ajedrez/Pieza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajedrez
+{
+    public abstract class Pieza
+    {
+        public Color Color { get; }
+        //public Posicion Posicion { get; set; }
+    }
+    public enum Color
+    {
+        Blanco,
+        Negro
+    }
+
+    //protected Pieza(Color color, Posicion posicion)
+    //{
+    //    Color = color;
+    //    posicion = posicion;
+    //}
+
+
+
+}
